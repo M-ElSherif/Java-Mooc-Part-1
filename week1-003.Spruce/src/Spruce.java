61f682fd9957e0986c5191abdeb7702af2118446
MoeS1990 <48065693+MoeS1990@users.noreply.github.com>
2019-03-03 21:33:40 -0500

    Add files via upload

---
+++ week1-003.Spruce/src/Spruce.java
@@ -0,0 +1,20 @@
+public class Spruce {
+
+    public static void main(String[] args) {
+
+        // Write code here
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+        System.out.println("    *");
+    }
+
+}
+//"    *",
+//        "   ***",
+//        "  *****",
+//        " *******",
+//        "*********",
+//        "    *"};
